b11f0759ae1371bff256070e263b7d34f83cd8f6
Cameron Steele <Cam.steeleis@gmail.com>
2017-10-26 11:07:47 -0500

    Test

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.json());
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var a = "";
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.post('/', function(request, response) {
+  a = request.body.hello;
+  response.end();
 });
 
+app.get('/', function(req,res){
+  res.send(a);
+})
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
